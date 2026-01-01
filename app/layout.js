3cb915b2eba1c36651bcdd837b11c12892ad2f8f
aastha-1511 <158474938+aastha-1511@users.noreply.github.com>
2026-01-01 19:06:04 +0530

    Import scrollFix component in layout.js

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import Header from "@/components/header";
+import Header from "@/components/scrollFix";
 import { ClerkProvider } from "@clerk/nextjs";
 import { Toaster } from "sonner";
 
@@ -23,6 +24,7 @@ export default function RootLayout({ children }) {
             enableSystem
             disableTransitionOnChange
           >
+            <scollFix />
             <Header />
 
             {/* MAIN CONTENT */}
